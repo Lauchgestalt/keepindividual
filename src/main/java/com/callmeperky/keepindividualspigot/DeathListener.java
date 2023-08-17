05d10a856bd665049cc47f88b534524e43f5dafb
Jonas Perkams <perkams@campus.tu-berlin.de>
2023-08-17 12:20:45 +0200

    Added random join/death messages

--- src/main/java/com/callmeperky/keepindividualspigot/DeathListener.java
+++ src/main/java/com/callmeperky/keepindividualspigot/DeathListener.java
@@ -4,11 +4,43 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DeathListener implements Listener {
     LuckPermsHandler luckperms = new LuckPermsHandler();
+    List<String> deathMessages = new ArrayList<String>();
+
+    public DeathListener(){
+        deathMessages.add("Schade, %player% ist gestorben.");
+        deathMessages.add("Oh nein, %player% ist gestorben.");
+        deathMessages.add("RIP %player%");
+        deathMessages.add("F in den Chat für %player%");
+        deathMessages.add("Oh nein! %player% hat das Zeitliche gesegnet.");
+        deathMessages.add("Ein Held fällt: %player%'s Abenteuer endet hier.");
+        deathMessages.add("Die Pixelwelt trauert um %player%'s Verlust.");
+        deathMessages.add("Trostloser Moment: %player% ist gefallen.");
+        deathMessages.add("Die Götter rufen nach %player%.");
+        deathMessages.add("Das Spiel der Blöcke hat %player% überlistet.");
+        deathMessages.add("Ein tragischer Tod für %player%, aber das Abenteuer geht weiter.");
+        deathMessages.add("Die Legende von %player% endet hier.");
+        deathMessages.add("Der Tapferkeit von %player% wird gedacht.");
+        deathMessages.add("Der Boden hat sich nach %player% gesehnt.");
+        deathMessages.add("Die Pixel haben ihren Tribut gefordert: %player% ist gefallen.");
+        deathMessages.add("Selbst die Mutigsten wie %player% können dem Tod nicht entkommen.");
+        deathMessages.add("Die Welt der Blöcke hat %player%'s Geschichte beendet.");
+        deathMessages.add("Die Schergen der Dunkelheit haben %player% ergriffen.");
+        deathMessages.add("Ein weiteres Abenteuer von %player% findet ein jähes Ende.");
+        deathMessages.add("Die Servergötter nehmen %player% in ihre Obhut.");
+        deathMessages.add("Die Schatten haben %player% verschlungen, doch die Erinnerung bleibt.");
+        deathMessages.add("%player% hat den Endbildschirm erreicht.");
+        deathMessages.add("Die Pixelwelt hat %player% verschlungen.");
+
+    }
+
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event){
-        event.setDeathMessage("Schade, " + event.getEntity().getName() + " ist gestorben.");
+        event.setDeathMessage(deathMessages.get((int) (Math.random() * deathMessages.size())).replace("%player%", event.getEntity().getName()));
         boolean perm = luckperms.api.getUserManager().getUser(event.getEntity().getUniqueId()).getCachedData().getPermissionData().checkPermission("keepinv.keep").asBoolean();
         if(perm){
             event.setKeepInventory(true);
--- src/main/java/com/callmeperky/keepindividualspigot/JoinListener.java
+++ src/main/java/com/callmeperky/keepindividualspigot/JoinListener.java
@@ -4,9 +4,39 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JoinListener implements Listener {
+    private List<String> joinMessages = new ArrayList<>();
+
+    public JoinListener(){
+        joinMessages.add("Schön, dass du da bist, %player% :)");;
+        joinMessages.add("Willkommen, %player%!");;
+        joinMessages.add("Hallo, %player%!");;
+        joinMessages.add("Juhu! %player% ist da!");;
+        joinMessages.add("Ein lautes 'Willkommen' für %player%!");
+        joinMessages.add("Der tapfere Abenteurer %player% ist eingetroffen!");
+        joinMessages.add("Hoch die Tassen, %player% ist da!");
+        joinMessages.add("Alle Mann an Deck, %player% ist an Land gesprungen!");
+        joinMessages.add("Herzlich willkommen, %player%! Die Party kann beginnen!");
+        joinMessages.add("Ahoi, %player%! Dein Abenteuer beginnt jetzt!");
+        joinMessages.add("Es ist Zeit, %player% zu begrüßen!");
+        joinMessages.add("Bereit machen für %player%'s epischen Auftritt!");
+        joinMessages.add("Rollt den roten Teppich aus für %player%!");
+        joinMessages.add("Ein donnerndes Hallo für %player%!");
+        joinMessages.add("Grüß dich, %player%! Deine Reise beginnt hier.");
+        joinMessages.add("Schick schick, %player% ist angekommen!");
+        joinMessages.add("Willkommen an Bord, %player%!");
+        joinMessages.add("Hey ho, %player%! Lasst das Abenteuer beginnen!");
+        joinMessages.add("Haltet euch fest, %player% ist gelandet!");
+        joinMessages.add("Schön dich zu sehen, %player%! Bring das Pixelabenteuer zum Leben!");
+        joinMessages.add("Herzlich willkommen, %player%! Deine Legende wartet auf dich!");
+        joinMessages.add("Gut gemacht, du hast den Server gefunden, %player%!");
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
-        event.setJoinMessage("Schön, dass du da bist, " + event.getPlayer().getName() + " :)");
+        event.setJoinMessage(joinMessages.get((int) (Math.random() * joinMessages.size())).replace("%player%", event.getPlayer().getName()));
     }
 }
