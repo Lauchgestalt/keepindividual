21f8a235347d1373fd5425e664c5228712915c60
Jonas Perkams <perkams@campus.tu-berlin.de>
2023-08-17 12:01:54 +0200

    Fixed Console Color

--- src/main/java/com/callmeperky/keepindividualspigot/KeepIndividualSpigot.java
+++ src/main/java/com/callmeperky/keepindividualspigot/KeepIndividualSpigot.java
@@ -1,17 +1,17 @@
 package com.callmeperky.keepindividualspigot;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KeepIndividualSpigot extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getServer().getConsoleSender().sendMessage(Color.GREEN + "KeepIndividualSpigot enabled.");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "KeepIndividualSpigot enabled.");
         boolean gameruleValue = Bukkit.getServer().getWorlds().get(0).getGameRuleValue("keepInventory").equals("true");
         if(gameruleValue){
-            Bukkit.getServer().getConsoleSender().sendMessage(Color.RED + "WARNING: gamerule keepInventory is set to true. This will override the plugins behavior.");
+            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "WARNING: gamerule keepInventory is set to true. This will override the plugins behavior.");
         }
 
         getServer().getPluginManager().registerEvents(new DeathListener(), this);
